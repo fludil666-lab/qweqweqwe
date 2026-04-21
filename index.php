04cdf3093a8ab1e6b82ea138989a97e306f413af
fludil666-lab <fludil666@gmail.com>
2026-04-21 16:55:36 +0400

    Рабочий на локалке

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Bootstrap the Laravel application from the project root.
+ *
+ * Some shared hosting environments use the project root as the
+ * document root. In that case, this file allows the application
+ * to run correctly without using serve.bat.
+ */
+
+require __DIR__ . '/public/index.php';
